95ad3a1b76ec44db476a2c96d0066feebf3f4e1d
Richard Neumann <r.neumann@homeinfo.de>
2017-07-13 14:00:48 +0200

    debug

--- backend/immobrowse.py
+++ backend/immobrowse.py
@@ -89,7 +89,10 @@ def barrierfree(portals):
     """Yields barrier free real estates"""
 
     for immobilie in Immobilie:
+        print('XXX:', portals, '-',
+              [immobilie.approve(portal for portal in portals)], end='\t')
         if approve(immobilie, portals):
+            print('NO')
             try:
                 barrier_freeness = immobilie.barrier_freeness
             except DoesNotExist:
@@ -97,6 +100,8 @@ def barrierfree(portals):
             else:
                 if barrier_freeness.complete or barrier_freeness.limited:
                     yield immobilie
+        else:
+            print('NO')
 
 
 def attachment(real_estate, ident):
