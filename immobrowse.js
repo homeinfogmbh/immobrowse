1cfa86e440b4b41d7b662a058019a812a8e03905
Richard Neumann <r.neumann@homeinfo.de>
2019-01-10 11:53:49 +0100

    fix

--- immobrowse.js
+++ immobrowse.js
@@ -798,7 +798,8 @@ immobrowse.RealEstate = function (json) {
     this.type = function () {
         for (var type of this.types()) {
             if (type != null) {
-                return homeinfo.str.capitalizeFirstLetter(type.toLowerCase());
+                type = type.toLowerCase();
+                return homeinfo.str.capitalizeFirstLetter(type);
             }
         }
 
