1778a1b2a6d2f9f356325560bba7c3e41f32a384
Richard Neumann <r.neumann@homeinfo.de>
2020-06-03 10:34:10 +0200

    Refactored Wordpress plugin for current API.

--- wordpress/homeinfo.php
+++ wordpress/homeinfo.php
@@ -1,107 +1,100 @@
 <?php
 /**
-* Plugin Name: Homeinfo ImmoBrowse
+* Plugin Name: HOMEINFO ImmoBrowse
 * Plugin URI: https://www.homeinfo.de/
-* Description: Homeinfo ImmoBrowse
-* Version: 1.1.4
+* Description: Anzeige Ihrer HOMEINFO Immobilien auf Ihrer Website
+* Version: 1.1.5
 **/
 
 // Make sure we don't expose any info if called directly
 if ( !function_exists( 'add_action' ) ) {
-        echo 'Hi there!  I\'m just a plugin, not much I can do when called directly.';
-        exit;
+	echo 'Hi there!  I\'m just a plugin, not much I can do when called directly.';
+	exit;
 }
 
-	include("settings.php");
+include("settings.php");
+add_shortcode('ImmoBrowse', 'immobrowse_shortcut');
 
+function immobrowse_shortcut() {
+	$options = get_option('ImmobrowseOptions');
 
-	add_shortcode('ImmoBrowse', 'immoBrowse_Shortcut');
+	if (isset( $_GET['real_estate'] ) ) {
 
-	function immoBrowse_Shortcut(){
-
-		$options = get_option('ImmobrowseOptions');
-
-		if(isset($_GET['real_estate'])) {
-
-			if (file_exists(plugin_dir_path( __FILE__ )."template/expose.inc")){
-				$content=file_get_contents(plugin_dir_path( __FILE__ )."template/expose.inc");
-				$content=str_replace("[BASEDIR]",plugins_url('/',__FILE__ ),$content);
-				$content=str_replace("[RECAPTCHA]",$options['recaptcha'],$content);
-			} else {
-				$content= "Datei expose.inc existiert nicht.";
-			}
-
+		if ( file_exists( plugin_dir_path( __FILE__ )."template/expose.inc" ) ){
+			$content = file_get_contents( plugin_dir_path( __FILE__ )."template/expose.inc" );
+			$content = str_replace( "[BASEDIR]", plugins_url('/',__FILE__ ), $content );
+			$content = str_replace( "[RECAPTCHA]", $options['recaptcha'], $content );
 		} else {
-
-			if (file_exists(plugin_dir_path( __FILE__ )."template/liste.inc")){
-					$content=file_get_contents(plugin_dir_path( __FILE__ )."template/liste.inc");
-			} else {
-				$content= "Datei liste.inc existiert nicht.";
-			}
+			$content = "Datei expose.inc existiert nicht.";
 		}
 
-		$content=
-			"<script>
-			jQuery.noConflict();
-				var customer=".$options['customerId'].";
-				var homeinfo_recaptcha='".$options['recaptcha']."';
-				var homeinfoBaseurl='".plugins_url('',__FILE__ )."/';
-				jQuery(document).ready(function($) { 
-					$( \".ib-image-frame, .homeinfo-contact,#furtherImages\" ).on( \"click\", function() {
-						setTimeout(function(){
+	} else {
 
-							$( \".modal-backdrop\" ).hide();
-							$( \".modal\" ).css('z-index',99999);
-
-						}, 1000);
-					});
-				});
-			</script>
-			".$content;
-
-		return $content;
-
+		if ( file_exists( plugin_dir_path( __FILE__ )."template/liste.inc" ) ){
+				$content = file_get_contents( plugin_dir_path( __FILE__ )."template/liste.inc" );
+		} else {
+			$content = "Datei liste.inc existiert nicht.";
+		}
 	}
+
+	$content =
+		"<script>
+		jQuery.noConflict();
+			var customer=".$options['customerId'].";
+			var homeinfo_recaptcha='".$options['recaptcha']."';
+			var homeinfoBaseurl='".plugins_url( '', __FILE__ )."/';
+			jQuery(document).ready(function($) {
+				$( \".ib-image-frame, .homeinfo-contact,#furtherImages\" ).on( \"click\", function() {
+					setTimeout(function(){
+
+						$( \".modal-backdrop\" ).hide();
+						$( \".modal\" ).css('z-index',99999);
+
+					}, 1000);
+				});
+			});
+		</script>
+		".$content;
+
+	return $content;
+}
 
 // remove wp version param from any enqueued scripts
-	function vc_remove_wp_ver_css_js( $src ) {
-		if ( strpos( $src, 'ver=' ) )
-			$src = remove_query_arg( 'ver', $src );
-		return $src;
+function vc_remove_wp_ver_css_js( $src ) {
+	if ( strpos( $src, 'ver=' ) )
+		$src = remove_query_arg( 'ver', $src );
+
+	return $src;
+}
+
+add_filter( 'style_loader_src', array( $this, 'vc_remove_wp_ver_css_js' ), 9999 );
+add_filter( 'script_loader_src', array( $this, 'vc_remove_wp_ver_css_js' ), 9999 );
+add_action( 'wp_enqueue_scripts', array( $this, 'immobrowse_setup_js') );
+
+function immobrowse_setup_js() {
+	foreach ( glob( plugin_dir_path( __FILE__ )."css/*.css") as $cssfile ){
+		$cssfile=basename($cssfile);
+		$cssClass=substr($cssfile, 0, -4);
+
+		wp_register_style( 'immobrowsecss-'.$cssClass, plugins_url( 'css/'.$cssfile,__FILE__ ) );
+		wp_enqueue_style( 'immobrowsecss-'.$cssClass );
 	}
-	add_filter( 'style_loader_src', 'vc_remove_wp_ver_css_js', 9999 );
-	add_filter( 'script_loader_src', 'vc_remove_wp_ver_css_js', 9999 );
 
+	foreach ( glob( plugin_dir_path( __FILE__ )."js/*.js" ) as $jsfile ){
+		$jsfile=basename($jsfile);
+		$jsClass=substr($jsfile, 0, -3);
+		$basenameWithoutPrefix=substr($jsfile, 3);
 
-	add_action('wp_enqueue_scripts', 'callback_for_setting_up_scripts');
-	function callback_for_setting_up_scripts() {
-
-		foreach (glob(plugin_dir_path( __FILE__ )."css/*.css") as $cssfile){
-
-			$cssfile=basename($cssfile);
-			$cssClass=substr($cssfile, 0, -4);
-
-			wp_register_style( 'immobrowsecss-'.$cssClass, plugins_url('css/'.$cssfile,__FILE__ ) );
-			wp_enqueue_style( 'immobrowsecss-'.$cssClass );
+		/* Load list.js and expose.js only when loading the respective site. */
+		if ( $basenameWithoutPrefix == "list.js" && isset( $_GET['real_estate'] ) ) {
+			continue;
+		} else if ( $basenameWithoutPrefix == "expose.js" && ! isset( $_GET['real_estate'] ) ) {
+			continue;
 		}
 
-		foreach (glob(plugin_dir_path( __FILE__ )."js/*.js") as $jsfile){
-
-			$jsfile=basename($jsfile);
-			$jsClass=substr($jsfile, 0, -3);
-			$basenameWithoutPrefix=substr($jsfile, 3);
-
-			/* Load list.js and expose.js only when loading the respective site. */
-			if ($basenameWithoutPrefix == "list.js" && isset($_GET['real_estate'])) {
-				continue;
-			} else if ($basenameWithoutPrefix == "expose.js" && ! isset($_GET['real_estate'])) {
-				continue;
-			}
-
-			wp_enqueue_script( 'immobrowsejs-'.$jsClass, plugins_url('js/'.$jsfile,__FILE__ ) );
-		}
-
-		wp_enqueue_script( 'immobrowsejs-recaptcha','https://www.google.com/recaptcha/api.js');
-
+		wp_enqueue_script( 'immobrowsejs-'.$jsClass, plugins_url( 'js/'.$jsfile,__FILE__ ) );
 	}
+
+	wp_enqueue_script( 'immobrowsejs-recaptcha','https://www.google.com/recaptcha/api.js' );
+}
 ?>
