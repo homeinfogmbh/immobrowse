fdb7df97ac6845b359b2f9df29f5e03509c4e9b1
Richard Neumann <mail@richard-neumann.de>
2021-02-03 12:35:36 +0100

    Migrate to setuptools.

--- backend/setup.py
+++ backend/setup.py
@@ -1,13 +1,19 @@
 #! /usr/bin/env python3
+"""Install script."""
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='immobrowse',
+    use_scm_version={
+        "local_scheme": "node-and-timestamp"
+    },
+    setup_requires=['setuptools_scm'],
     author='HOMEINFO - Digitale Informationssysteme GmbH',
-    author_email='info@homeinfo.de',
+    author_email='<info at homeinfo dot de>',
     maintainer='Richard Neumann',
-    maintainer_email='r.neumann@homeinfo.de',
+    maintainer_email='<r dot neumann at homeinfo period de>',
     requires=['openimmodb'],
     packages=['immobrowse'],
-    description='Real estate search engine.')
+    description='Real estate search engine.'
+)
