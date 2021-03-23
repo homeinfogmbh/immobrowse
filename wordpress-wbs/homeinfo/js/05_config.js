f36ab3018e659780fdd30787c99e5ce7c74e4d5e
Richard Neumann <mail@richard-neumann.de>
2021-03-23 16:28:28 +0100

    Migrate to arrow function

--- wordpress-wbs/homeinfo/js/05_config.js
+++ wordpress-wbs/homeinfo/js/05_config.js
@@ -7,7 +7,5 @@ immobrowse.config = immobrowse.config || {};
 immobrowse.config.types = ['WOHNUNG'];
 immobrowse.config.marketing = ['MIETE_PACHT'];
 immobrowse.config.shortFloorNames = true;
-immobrowse.config.exposeURLCallback = function (objectId) {
-    return '?real_estate=' + objectId;
-};
+immobrowse.config.exposeURLCallback = objectId => '?real_estate=' + objectId;
 immobrowse.config.addressInList = true;
