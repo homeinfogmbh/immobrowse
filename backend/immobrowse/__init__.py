33e15d7caa0b1b19dfc0b2ade22162d0a0cb3200
Richard Neumann <r.neumann@homeinfo.de>
2022-01-06 11:34:22 +0100

    Remove local backend

--- backend/immobrowse/__init__.py
+++ backend/immobrowse/__init__.py
@@ -2,7 +2,6 @@
 
 from immobrowse.barrierfree import APPLICATION as BARRIERFREE
 from immobrowse.immobrowse import APPLICATION as IMMOBROWSE
-from immobrowse.local import APPLICATION as LOCAL
 
 
-__all__ = ['BARRIERFREE', 'IMMOBROWSE', 'LOCAL']
+__all__ = ['BARRIERFREE', 'IMMOBROWSE']
